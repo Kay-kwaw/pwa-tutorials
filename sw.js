04a5ac19da0184be116fe1af0b59651c0a6da8a2
kwaw-kumi <kwawkumi9@gmail.com>
2023-02-20 21:53:23 -0800

    lighthousing

--- sw.js
+++ sw.js
@@ -1,5 +1,9 @@
 self.addEventListener('install', evt => {
     console.log('service installed');
 });
-
 //how to install services worker
+
+//How to activate services (event listener)
+self.addEventListener('activate', evt => {
+    console.log('Event service worker activated');
+});
